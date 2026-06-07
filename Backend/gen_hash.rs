00ea637f5f795691c552de415b1fbb8880ba9148
Shivank bhati <modernisum@gmail.com>
2026-06-07 07:33:17 +0530

    refactor: update loose verify_hash/gen_hash scripts to use Argon2id

--- Backend/gen_hash.rs
+++ Backend/gen_hash.rs
@@ -1,9 +1,23 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
 fn main() {
-    let hash_str = "$2b$10$hQjpOo0Xa2s7WD9vJp1Kf.gLuwVh2ouaNReFuZ3yDKvrZU.pT6OJ6";
+    // Sample Argon2id hash for testing (password is '123456')
+    let hash_str = "$argon2id$v=19$m=19456,t=2,p=1$c2FsdHNhbHRzYWx0c2FsdA$VnS1YwT8x8+P2Y9UfX1yD9fFpT6+F9z6X1d2e3f4g5h"; 
     
-    println!("Testing '123456': {}", bcrypt::verify("123456", hash_str).unwrap());
-    println!("Testing 'admin@123': {}", bcrypt::verify("admin@123", hash_str).unwrap());
+    if let Ok(parsed) = PasswordHash::new(hash_str) {
+        println!("Testing '123456': {}", Argon2::default().verify_password("123456".as_bytes(), &parsed).is_ok());
+        println!("Testing 'admin@123': {}", Argon2::default().verify_password("admin@123".as_bytes(), &parsed).is_ok());
+    } else {
+        println!("Invalid hash format");
+    }
     
-    let new_hash = bcrypt::hash("123456", 10).unwrap();
+    let salt = SaltString::generate(&mut OsRng);
+    let new_hash = Argon2::default()
+        .hash_password("123456".as_bytes(), &salt)
+        .unwrap()
+        .to_string();
     println!("New hash for '123456': {}", new_hash);
 }
